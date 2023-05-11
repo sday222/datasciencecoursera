8f7ae56a7adb9b00a913d7c47d3db4b17a5e91d2
sday222 <sean.m.day@gmail.com>
2023-05-11 09:54:02 -0600

    solved Part 2

--- R Programming/Week 2/assignment1.R
+++ R Programming/Week 2/assignment1.R
@@ -15,10 +15,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
 complete <- function(directory, id = 1:332) {
   complete_files <- list.files(directory, full.names = TRUE)
   
-  complete_data <- data.frame()
+  complete_data <- data.frame(id=numeric(0), nobs=numeric(0))
   for (i in id) {
-    complete_data <- rbind(complete_data, read.csv(complete_files[i]))
+    single_file <- data.frame(id=numeric(0), nobs=numeric(0))
+    single_file$id <- i
+    single_file$nobs <- sum(complete.cases(read.csv(complete_files[i])))
+    
+    complete_data <- rbind(complete_data, single_file)
   }
-  
-  sum(complete.cases(complete_data))
 }
+
+
+# get the files
+# create the externallly accessed data model
+
+# inside of the loop grab the Id of the current file
+# add that information to a data table that exists only inside of the loop
+
+# 
