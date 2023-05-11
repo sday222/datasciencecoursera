01f35c250b6e32d2d4362a2e552cbbea0971f881
Sean Day <sean.m.day@gmail.com>
2023-05-11 07:49:04 -0600

    second function started

--- R Programming/Week 2/assignment1.R
+++ R Programming/Week 2/assignment1.R
@@ -1,3 +1,4 @@
+# Part 1
 pollutantmean <- function(directory, pollutant, id = 1:332) {
   pollutant_files <- list.files(directory, full.names = TRUE)
   
@@ -7,4 +8,17 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   }
   
   mean(pollutant_data[,pollutant], na.rm = TRUE)
+}
+
+
+# Part 2
+complete <- function(directory, id = 1:332) {
+  complete_files <- list.files(directory, full.names = TRUE)
+  
+  complete_data <- data.frame()
+  for (i in id) {
+    complete_data <- rbind(complete_data, read.csv(complete_files[i]))
+  }
+  
+  sum(complete.cases(complete_data))
 }
